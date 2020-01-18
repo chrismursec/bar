e6ceb74ea1d7e11c6979e3ff417c210531e3913a
Chris Mursec <christophermursec@gmail.com>
2020-01-18 17:48:33 +0000

    added charging to battery component

--- elements/battery.jsx
+++ elements/battery.jsx
@@ -1,55 +1,70 @@
 import { element } from '../lib/style.js';
 
-const render = ({ output, error, side, config, data }) => {
-  var batColor = (level) => {
-    var level = parseInt(level)
+const render = ({ output, error, side, config, data, charge }) => {
+	var batColor = level => {
+		var level = parseInt(level);
 
-    if (level > 80)
-      return "#97c475"; // Green
-    else if (level > 55)
-      return "#e5c07b"; // Yellow
-    else if (level > 30)
-      return "#d09a6a"; // Orange
-    return "#e06c75";   // Red
-  }
+		if (level > 80) return '#97c475';
+		// Green
+		else if (level > 55) return '#e5c07b';
+		// Yellow
+		else if (level > 30) return '#d09a6a'; // Orange
+		return '#e06c75'; // Red
+	};
 
-  var style = (level) => {
-    return {
-      ...element,
-      ...config.style,
-      float: side,
-      color: batColor(level)
-    }
-  }
+	var style = level => {
+		return {
+			...element,
+			...config.style,
+			float: side,
+			color: batColor(level)
+		};
+	};
 
-  var iconStyle = {
-    padding: '0 0 0 10px',
-    fontSize: '16px'
-  }
+	var iconStyle = {
+		padding: '0 0 0 10px',
+		fontSize: '16px'
+	};
 
-  var iconName = (level) => {
-    var level = parseInt(level)
-    if (level > 80)
-      return "battery-full"
-    if (level > 60)
-      return "battery-three-quarters"
-    if (level > 40)
-      return "battery-half"
-    if (level > 20)
-      return "battery-quarter"
-    return "battery-empty"
-  }
+	var charging = charge => {
+		var isCharging = false;
+		if (charge === "'AC Power'") {
+			isCharging = true;
+		}
+		return isCharging;
+	};
 
-  return error || data == 0 ? (
-    <span style={style(0)}></span>
-  ) : (
-    <span style={style(data)}>
-      <span>{data}</span>
-      <span style={iconStyle}>
-        <i className={'far fa-' + iconName(data)}></i>
-      </span>
-    </span>
-  )
-}
+	var showBolt = () => {
+		if (charging(charge)) return 'bolt';
+	};
 
-export default render
+	var iconName = level => {
+		var level = parseInt(level);
+		if (level > 80) return 'battery-full';
+		if (level > 60) return 'battery-three-quarters';
+		if (level > 40) return 'battery-half';
+		if (level > 20) return 'battery-quarter';
+		return 'battery-empty';
+	};
+
+	return error || data == 0 ? (
+		<span style={style(0)}></span>
+	) : (
+		<span style={style(data)}>
+			<span>{data}</span>
+
+			<span> {<i className={'far fa-' + charging(charge)}></i>}</span>
+			<span style={iconStyle}>
+				<i className={'far fa-' + iconName(data)}></i>
+			</span>
+			<span style={iconStyle}>
+				<i
+					className={'far fa-' + showBolt()}
+					style={{ color: '#97c475' }}
+				></i>
+			</span>
+		</span>
+	);
+};
+
+export default render;
--- index.jsx
+++ index.jsx
@@ -1,66 +1,73 @@
 // Update every second for the clock. Expensive elements should
 // throttle themselves
-export const refreshFrequency = 5000 // ms
+export const refreshFrequency = 5000; // ms
 
 import { theme } from './lib/style.js';
-import {
-  Battery,
-  Cpu,
-  Time,
-  Workspaces,
-  Playing
-} from './elements/index.jsx'
+import { Battery, Cpu, Time, Workspaces, Playing } from './elements/index.jsx';
 
 const config = {
-  time: {
-    format: "%l:%M",
-    style: {
-      padding: '0 15px',
-      backgroundColor: theme.backgroundLight,
-    }
-  },
-  battery: {
-    style: {}
-  },
-  workspaces: {
-    style: {}
-  },
-  cpu: {
-    style: {}
-  },
-  playing: {
-    style: {}
-  }
-}
+	time: {
+		format: '%l:%M',
+		style: {
+			padding: '0 15px',
+			backgroundColor: theme.backgroundLight
+		}
+	},
+	battery: {
+		style: {}
+	},
+	workspaces: {
+		style: {}
+	},
+	cpu: {
+		style: {}
+	},
+	playing: {
+		style: {}
+	}
+};
 
 const barStyle = {
-  top: 0,
-  right: 0,
-  left: 0,
-  position: 'fixed',
-  background: theme.background,
-  overflow: 'hidden',
-  color: theme.text,
-  height: '25px',
-  fontFamily: 'Helvetica',
-  fontSize: '.9rem',
-  boxShadow: '0px 2px 5px 0 #000000',
-}
-
+	bottom: 0,
+	right: 0,
+	left: 0,
+	position: 'fixed',
+	background: theme.background,
+	overflow: 'hidden',
+	color: theme.text,
+	height: '25px',
+	fontFamily: 'Helvetica',
+	fontSize: '.9rem',
+	boxShadow: '0px 2px 5px 0 #000000'
+};
 
 const result = (data, key) => {
-  try {
-    return JSON.parse(data)[key]
-  } catch (e) {
-    return ''
-  }
-}
+	try {
+		return JSON.parse(data)[key];
+	} catch (e) {
+		return '';
+	}
+};
 
 // export const command = 'sh bar/scripts/update'
 export const command = `
 BAT=$(pmset -g batt | egrep '([0-9]+\%).*' -o --colour=auto | cut -f1 -d';');
+CHARGE=$(pmset -g batt | egrep "'([^']+).*'" -o --colour=auto |cut -f1 -d';');
 SPACE=$(if command -v /usr/local/bin/chunkc >/dev/null 2>&1; then echo $(/usr/local/bin/chunkc tiling::query -d id); else echo ""; fi)
-SPOTIFY=$(osascript -e 'tell application "System Events"set processList to (name of every process)end tellif (processList contains "Spotify") is true thentell application "Spotify"if player state is playing thenset artistName to artist of current trackset trackName to name of current trackreturn artistName & " - " & trackNameelsereturn ""end ifend tellend if')
+SPOTIFY=$(osascript -e 'tell application "System Events"
+set processList to (name of every process)
+end tell
+if (processList contains "Spotify") is true then
+tell application "Spotify"
+if player state is playing then
+set artistName to artist of current track
+set trackName to name of current track
+return artistName & " - " & trackName
+else
+return ""
+end if
+end tell
+end if')
 
 
 echo $(cat <<-EOF
@@ -71,27 +78,38 @@ echo $(cat <<-EOF
   }
 EOF
 );
-`
+`;
 
 export const render = ({ output, error }) => {
-  if(error) {
-    console.log(new Date())
-    console.log(error)
-    console.log(String(error))
-  }
-  let errorContent = (
-    <div style={barStyle}></div>
-  )
-  let content = (
-    <div style={barStyle}>
-      <link rel="stylesheet" type="text/css" href="bar/assets/font-awesome/css/all.min.css" />
-      <Workspaces config={config.workspaces} data={result(output, "workspace")} side="left" />
+	if (error) {
+		console.log(new Date());
+		console.log(error);
+		console.log(String(error));
+	}
+	let errorContent = <div style={barStyle}></div>;
+	let content = (
+		<div style={barStyle}>
+			<link
+				rel="stylesheet"
+				type="text/css"
+				href="bar/assets/font-awesome/css/all.min.css"
+			/>
+			<Workspaces
+				config={config.workspaces}
+				data={result(output, 'workspace')}
+				side="left"
+			/>
 
-      <Playing config={config.playing} data={result(output, "playing")} />
+			<Playing config={config.playing} data={result(output, 'playing')} />
 
-      <Time config={config.time} side="right"></Time>
-      <Battery config={config.battery} data={result(output, "battery")} side="right" />
-    </div>
-  )
-  return error ? errorContent : content
-}
+			<Time config={config.time} side="right"></Time>
+			<Battery
+				config={config.battery}
+				data={result(output, 'battery')}
+				charge={result(output, 'charging')}
+				side="right"
+			/>
+		</div>
+	);
+	return error ? errorContent : content;
+};
